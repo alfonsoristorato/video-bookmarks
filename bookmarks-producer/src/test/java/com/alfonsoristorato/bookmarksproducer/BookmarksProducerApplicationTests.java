4d705e84abeb4f451728b231cc4d52492e10a01d
alfonsoristorato <alfonsoristorato@gmail.com>
2023-06-22 19:48:44 +0100

    created producer module

---
+++ bookmarks-producer/src/test/java/com/alfonsoristorato/bookmarksproducer/BookmarksProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfonsoristorato.bookmarksproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarksProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
