298db2afeba4abfcb7c782b2e8c13741564e4a68
alfonsoristorato <alfonsoristorato@gmail.com>
2023-06-28 17:11:00 +0100

    created module persister and added cassandra to docker-compose.yml

---
+++ bookmarks-persister/app/src/main/java/com/alfonsoristorato/bookmarkspersister/app/BookmarksPersisterApplication.java
@@ -0,0 +1,11 @@
+package com.alfonsoristorato.bookmarkspersister.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarksPersisterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarksPersisterApplication.class, args);
+    }
+}
