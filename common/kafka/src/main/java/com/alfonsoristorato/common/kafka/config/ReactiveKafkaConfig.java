54a7b29a53529638da2321790016596f94e1235d
alfonsoristorato <alfonsoristorato@gmail.com>
2023-07-10 09:24:39 +0100

    added lazy loading for kafka consumer and producer beans

--- common/kafka/src/main/java/com/alfonsoristorato/common/kafka/config/ReactiveKafkaConfig.java
+++ common/kafka/src/main/java/com/alfonsoristorato/common/kafka/config/ReactiveKafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
@@ -22,12 +23,14 @@ public class ReactiveKafkaConfig {
         this.kafkaTopicConfigProperties = kafkaTopicConfigProperties;
     }
 
+    @Lazy
     @Bean
     ReactiveKafkaProducerTemplate<String, String> reactiveKafkaProducerTemplate(KafkaProperties properties) {
         Map<String, Object> props = properties.buildProducerProperties();
         return new ReactiveKafkaProducerTemplate<>(SenderOptions.create(props));
     }
 
+    @Lazy
     @Bean
     ReactiveKafkaConsumerTemplate<String, String> reactiveKafkaConsumerTemplate(KafkaProperties properties) {
         Map<String, Object> props = properties.buildProducerProperties();
